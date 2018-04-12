691b1a87d2745c97cd587a51348db2831ac5afdf
Soldream <nonemail@mail.ru>
2018-04-13 00:09:33 +0300

    NYA v0.0.3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const robot = new Discord.Client();
 robot.login("NDMzNzI2NTMxNTUwODM4Nzg5.DbAFOw._cmsVxdWHC5OTnY_azsXE97qudE");
 
 robot.on('message',(message) => {
-    if(message.content == "торт1") {
+    if(message.content == "торт2") {
         message.channel.send("Да, я люблю торты!");
     }
 });
